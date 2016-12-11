e89668a34b3ccc0ccbcf16b59afceff264981cce
Will Strinz <wstrinz@gmail.com>
2016-12-11 12:56:36 -0600

    rough diff for forecast changes

--- lib/weatherbot/forecast_updater.ex
+++ lib/weatherbot/forecast_updater.ex
@@ -10,8 +10,21 @@ defmodule Weatherbot.ForecastUpdater do
     |> Repo.all
   end
 
+  def new_sections(section_list, station) do
+    if station.last_forecast do
+      current_sections = String.split(station.last_forecast, "&&")
+      Enum.reject(section_list, fn sect -> Enum.any?(current_sections, &(String.strip(&1) == String.strip(sect))) end)
+    else
+      section_list
+    end
+  end
+
+  def latest_forecast_sections(station) do
+    Fetcher.get_section_list(station.code, ignored_strings_for(station)) |> new_sections(station)
+  end
+
   def latest_forecast(station) do
-    Fetcher.get_section_list(station.code, ignored_strings_for(station)) |> Enum.join("\n&&\n")
+    latest_forecast_sections(station) |> Enum.join("\n&&\n")
   end
 
   def update_station(station) do
